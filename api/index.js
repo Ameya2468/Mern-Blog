e48b0edc04b4576f65a8da6eae614fb2d2b53c21
Ameya <ameyakulkarni2004@gmail.com>
2026-02-02 01:12:04 +0530

    create signup api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import connectToDb from './db/db.js';
+import authRoutes from './routes/auth.route.js';
 import { setServers } from 'node:dns/promises';
 
 setServers(['1.1.1.1', '8.8.8.8']);
@@ -16,5 +17,6 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-
+app.use(express.json());
 app.use('/user',userRoutes);
+app.use('/auth',authRoutes);
