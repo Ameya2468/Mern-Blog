d47802fd4202e7f4bf53fe41d40ba533f45caa57
Ameya <ameyakulkarni2004@gmail.com>
2026-02-02 03:43:43 +0530

    created middleware and errorhandler to handle errors

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/user.route.js';
 import connectToDb from './db/db.js';
 import authRoutes from './routes/auth.route.js';
 import { setServers } from 'node:dns/promises';
+import { stat } from 'node:fs';
 
 setServers(['1.1.1.1', '8.8.8.8']);
 dotenv.config();
@@ -20,3 +21,13 @@ app.listen(3000, () => {
 app.use(express.json());
 app.use('/user',userRoutes);
 app.use('/auth',authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
