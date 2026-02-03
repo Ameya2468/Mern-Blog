b28aa892e0ac0036e29218428d7147ca7bf7b5ff
Ameya <ameyakulkarni2004@gmail.com>
2026-02-04 03:10:27 +0530

    create sign-in page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import  { Button,Label,TextInput } from 'flowbite-react'
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row
+      md-items-center gap-6'>
+        {/* Left Section */}
+        <div className='flex-1 mt-40'>
+          <Link to="/" className="font-bold dark:text-black text-4xl">
+            <span className='px-2 py-1 bg-gradient-to-r 
+            from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Ameya's</span>
+            Blog
+        </Link>
+        <p className='text-sm mt-5'>
+          You can sign up with your email
+           and password or With Google.
+        </p>
+        </div>
+        {/* Right Section */}
+        <div className='flex-1 mt-20'>
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label className='text-lg' htmlFor='username' color='black'>Your username</Label>
+              <TextInput  type='text' placeholder='Username' id='username'/>
+            </div>
+
+            <div>
+              <Label className="text-lg"htmlFor='email' color='black'>Your email</Label>
+              <TextInput type='text' placeholder='Email' id='email'/>
+            </div>
+
+            <div>
+              <Label className='text-lg' htmlFor='password' color='black'>Your password</Label>
+              <TextInput type='text' placeholder='Password' id='password'/>
+            </div>
+            {/* <Button gradientDuoTone="purpleToPink" type='submit'>Sign Up</Button> */}
+            <Button className='bg-gradient-to-r from-purple-500 to-pink-500 text-white' type='submit'>Sign Up</Button>
+          </form>
+          <div className='flex gap-2 text-md mt-5'>
+            <span>Have an account? </span>
+            <Link to="/sign-in" className='text-blue-500'>Sign in</Link>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Signin() {
   return (
-    <div>Signin</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto'>
+        {/* Left Section */}
+
+      </div>
+
+    </div>
   )
 }
