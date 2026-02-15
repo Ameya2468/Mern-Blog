d8db8b729cb91b1d3991a9b52da0d23b080267b7
Ameya <ameyakulkarni2004@gmail.com>
2026-02-15 18:18:53 +0530

    create sign in ui

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,10 +1,100 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useState } from 'react'
+import { Link, Navigate } from 'react-router-dom'
+import  { Alert, Button,Label,TextInput,Spinner } from 'flowbite-react'
+import { useNavigate } from 'react-router-dom';  
 
-export default function Signin() {
+export default function SignIn() {
+  const [formData, setFormData] = useState({username: '', email: '', password: ''});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value.trim()
+    });
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.email || !formData.password) {
+      return setErrorMessage("All fields are required");
+      
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false){
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/');
+      }
+    }catch (error) {
+      setLoading(false);
+      return setErrorMessage(error.message);
+      setLoading(false);
+    }}
   return (
     <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row
+      md-items-center gap-6'>
         {/* Left Section */}
+        <div className='flex-1 mt-40'>
+          <Link to="/" className="font-bold dark:text-black text-4xl">
+            <span className='px-2 py-1 bg-gradient-to-r 
+            from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Ameya's</span>
+            Blog
+        </Link>
+        <p className='text-sm mt-5'>
+          You can sign in with your email
+           and password or With Google.
+        </p>
+        </div>
+        {/* Right Section */}
+        <div className='flex-1 mt-20'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label className="text-lg"htmlFor='email' color='black'>Your email</Label>
+              <TextInput type='email' placeholder='Email' id='email' onChange={handleChange}/>
+            </div>
+
+            <div>
+              <Label className='text-lg' htmlFor='password' color='black'>Your password</Label>
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
+            </div>
+            {/* <Button gradientDuoTone="purpleToPink" type='submit'>Sign Up</Button> */}
+            <Button className='bg-gradient-to-r from-purple-500 to-pink-500 text-white' type='submit' disabled={loading}>
+              {loading ? (
+                <>
+                <Spinner size='sm'/>
+                <span className='pl-3'>Loading...</span>
+                </>
+                
+              ): ("Sign In")}
+            </Button>
+          </form>
+          <div className='flex gap-2 text-md mt-5'>
+            <span>Don't have an account? </span>
+            <Link to="/sign-up" className='text-blue-500'>Sign Up</Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert color="failure" className='mt-5'>{errorMessage}</Alert>
+            )
+          }
+        </div>
 
       </div>
 
